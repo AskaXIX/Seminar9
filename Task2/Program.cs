876d0e27e2adcb603c65dd95ec16131fffe8ce2b
Aslan <aslan.aslan.14@bk.ru>
2023-06-11 12:04:39 +0600

    Task2 done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int SumNumbers(int n, int m)
+{
+    if (n == m) return n;
+    else return SumNumbers(n + 1, m) + n;
+}
+
+int n = ReadInt("Input N: ");
+int m = ReadInt("Input M: ");
+
+System.Console.WriteLine(SumNumbers(n, m));
